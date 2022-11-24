f96617c8eaff3ef9c6386c67cc5d34ae9bcaf05c
min-jung <110580880+mj-gamza@users.noreply.github.com>
2022-11-24 15:22:58 +0900

    Update Article.java

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -7,7 +7,7 @@ public class Article {
     private Long id;
     private String title; // 제목
     private String content; // 본문
-    private String hashtab; // 해시태그
+    private String hashtag; // 해시태그
 
     private LocalDateTime createdAt; // 생성일시
     private String createdBy; // 생성자
